4d907ba89016782e2fd92d9aa65a2fc33feba04b
piyumiprabhashini11 <piyumimadduamge1700@gmail.com>
2025-11-25 13:28:51 +0000

    TestNG configuration

--- src/test/java/examples/smoke/SmokeTest.java
+++ src/test/java/examples/smoke/SmokeTest.java
@@ -2,15 +2,14 @@ package smoke;
 import org.testng.Assert;
 import org.testng.annotations.Test;
 
+@Test
 public class SmokeTest {
 
-    @Test
     public void verifyHomePageLoads() {
         System.out.println("Running Smoke Test: Verify Home Page Loads...");
         Assert.assertTrue(true, "Home page should load successfully");
     }
 
-    @Test
     public void verifyLoginButtonPresent() {
         System.out.println("Running Smoke Test: Verify Login Button...");
         Assert.assertTrue(true, "Login button should be visible");
--- src/waits/ExplicitWaitExample.java
+++ src/waits/ExplicitWaitExample.java
@@ -1,6 +1,7 @@
 package waits;
 
 import org.openqa.selenium.By;
+
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
